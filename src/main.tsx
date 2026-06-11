588245f313089c3557ec69765a2fae9f68ae7477
Pratik Nesarkar <pratik.nesarkar@tamu.edu>
2026-06-10 21:33:59 -0500

    feat: refactor styles by modularizing CSS into separate files for better organization and maintainability

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/theme.css'
+import './styles/base.css'
+import './styles/header.css'
+import './styles/form.css'
+import './styles/card.css'
+import './styles/countdown.css'
+import './styles/utilities.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
